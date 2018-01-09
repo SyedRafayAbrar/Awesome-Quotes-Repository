02488895c5172c51827c58450174a7773c9cdec2
SyedRafay <abrarrafay96@gmail.com>
2018-01-09 14:12:34 +0500

    third commit

--- QuotesCollection/ViewController.swift
+++ QuotesCollection/ViewController.swift
@@ -9,10 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController,iCarouselDataSource,iCarouselDelegate {
-var quotes = [UIImage]()
+    @IBOutlet var viewCarousel: iCarousel!
+var quotesImage = [UIImage]()
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        viewCarousel.type = .invertedWheel
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        quotesImage = [#imageLiteral(resourceName: "q1"),#imageLiteral(resourceName: "q1"),#imageLiteral(resourceName: "q1")]
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,20 +29,24 @@ var quotes = [UIImage]()
     }
 
     func numberOfItems(in carousel: iCarousel) -> Int {
-        return quotes.count
+        return quotesImage.count
     }
     
     func carousel(_ carousel: iCarousel, viewForItemAt index: Int, reusing view: UIView?) -> UIView {
-        let tempView = UIView()
-        
+        let tempView = UIView(frame: CGRect(x: 0, y: 0, width: 250, height: 350))
+            
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 250, height: 350))
+        var newimg = quotesImage[index]
+        button.setImage(newimg, for: .normal)
+        tempView.addSubview(button)
         return tempView
     }
     
     func carousel(_ carousel: iCarousel, valueFor option: iCarouselOption, withDefault value: CGFloat) -> CGFloat {
         if option == iCarouselOption.spacing{
-            return value*1.5
-        } else if option == iCarouselOption.spacing{
-            return value*(-0.5)
+            return value*1.2
+        } else if option == iCarouselOption.arc{
+            return value*(-0.1)
         }
     return value
     }
