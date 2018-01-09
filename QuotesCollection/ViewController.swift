9a4b510d392499a98bc01079dcc4876b1ade51f9
SyedRafay <abrarrafay96@gmail.com>
2018-01-09 13:52:44 +0500

    second commit

--- QuotesCollection/ViewController.swift
+++ QuotesCollection/ViewController.swift
@@ -8,8 +8,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,iCarouselDataSource,iCarouselDelegate {
+var quotes = [UIImage]()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +20,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func numberOfItems(in carousel: iCarousel) -> Int {
+        return quotes.count
+    }
+    
+    func carousel(_ carousel: iCarousel, viewForItemAt index: Int, reusing view: UIView?) -> UIView {
+        let tempView = UIView()
+        
+        return tempView
+    }
+    
+    func carousel(_ carousel: iCarousel, valueFor option: iCarouselOption, withDefault value: CGFloat) -> CGFloat {
+        if option == iCarouselOption.spacing{
+            return value*1.5
+        } else if option == iCarouselOption.spacing{
+            return value*(-0.5)
+        }
+    return value
+    }
+    
 
 }
 
